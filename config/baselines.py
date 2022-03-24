3be0d676930dc8028bb198bc9cc7dacb293398b1
vladimirrim <vladimirrim98@gmail.com>
2022-03-24 19:59:32 +0300

    Add training configs

---
+++ config/baselines.py
@@ -0,0 +1,29 @@
+import nmmo
+
+from config import bases, scale
+
+
+class Medium(scale.Baseline, bases.Medium, nmmo.config.AllGameSystems):
+    # Load 1000 epoch pretrained model
+    RESTORE = True
+    RESTORE_ID = '870d'
+
+    @property
+    def SPAWN(self):
+        return self.SPAWN_CONCURRENT
+
+    pass
+
+
+class Debug(scale.Debug, bases.Small, nmmo.config.AllGameSystems):
+    RESTORE = False
+
+    TRAINING_ITERATIONS = 2
+
+    SGD_MINIBATCH_SIZE = 100
+    TRAIN_BATCH_SIZE = 400
+    TRAIN_HORIZON = 200
+    EVALUATION_HORIZON = 50
+
+    HIDDEN = 2
+    EMBED = 2
