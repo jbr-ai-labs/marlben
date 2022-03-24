a10247bb5f6e31a33ac8613560408ce3db62e87b
vladimirrim <vladimirrim98@gmail.com>
2022-03-24 20:18:19 +0300

    Add working demo

--- src/environments/corridor/config.py
+++ src/environments/corridor/config.py
@@ -1,39 +1,24 @@
 import nmmo
-from scripted import baselines
+from config import scale
+from config.bases import Small
+from nmmo.config import build_from_dict
+from nmmo.core.spawn.custom_managers import CustomNPCManager, CustomPlayerManager
 from .map_generator import CorridorMapGenerator
-from src.core.custom_managers import CustomNPCManager, CustomPlayerManager
 
+config_dict = {"presets_list": [nmmo.config.AllGameSystems, Small, scale.Debug],
+               "parameters": {"AGENTS": [nmmo.Agent, nmmo.Agent],
+                              "TERRAIN_CENTER": 5,
+                              "NENT": 2,
+                              "NMOB": 0,
+                              "NSTIM": 1,
+                              "TERRAIN_BORDER": 2,
+                              "PATH_MAPS": 'src/environments/corridor/maps',
+                              "SPAWN_PARAMS": {'type': 'in_range',
+                                               'r_ranges': [(3, 5), (3, 5)],
+                                               'c_ranges': [(3, 3), (5, 5)]},
+                              "PLAYER_MANAGER": CustomPlayerManager,
+                              "NPC_MANAGER": CustomNPCManager,
+                              "MAP_GENERATOR": CorridorMapGenerator,
+                              "FORCE_MAP_GENERATION": False}}
 
-
-class Config(nmmo.config.Small, nmmo.config.AllGameSystems):
-    """Config objects subclass a nmmo.config.{Small, Medium, Large} template
-
-    Can also specify config game systems to enable various features"""
-
-    # Agents will be instantiated using templates included with the baselines
-    # Meander: randomly wanders around
-    # Forage: explicitly searches for food and water
-    # Combat: forages and actively fights other agents
-    AGENTS = [baselines.Meander, baselines.Meander]
-
-    TERRAIN_CENTER = 5
-    NENT = 2
-    NMOB = 0
-    NSTIM = 1
-    TERRAIN_BORDER = 2
-
-    # Set a unique path for demo maps
-    PATH_MAPS = 'src/environments/corridor/maps'
-    SPAWN_PARAMS = {
-        'type': 'in_range',
-        'r_ranges': [(3, 5), (3, 5)],
-        'c_ranges': [(3, 3), (5, 5)]
-    }
-    PLAYER_MANAGER = None
-    NPC_MANAGER = None
-
-    # Force terrain generation -- avoids unexpected behavior from caching
-    FORCE_MAP_GENERATION = False
-    MAP_GENERATOR = CorridorMapGenerator
-    PLAYER_MANAGER = CustomPlayerManager
-    NPC_MANAGER = CustomNPCManager
+corridor_config = build_from_dict(config_dict)
