834339601bf8f76c265babb3f4999d36263cf517
vladimirrim <vladimirrim98@gmail.com>
2022-04-29 13:46:41 +0300

    Refactoring

--- nmmo/entity/player.py
+++ nmmo/entity/player.py
@@ -1,91 +1,86 @@
-import numpy as np
-from pdb import set_trace as T
-
 import nmmo
-from nmmo.systems import ai, equipment
-from nmmo.lib import material
-
-from nmmo.systems.skill import Skills
-from nmmo.systems.achievement import Diary
 from nmmo.entity import entity
+from nmmo.systems.achievement import Diary
+from nmmo.systems.skill import Skills
+
 
 class Player(entity.Entity):
-   def __init__(self, realm, pos, agent, color, pop):
-      super().__init__(realm, pos, agent.iden, agent.name, color, pop)
+    def __init__(self, realm, pos, agent, color, pop):
+        super().__init__(realm, pos, agent.iden, agent.name, color, pop)
 
-      self.agent  = agent
-      self.pop    = pop
+        self.agent = agent
+        self.pop = pop
 
-      #Scripted hooks
-      self.target = None
-      self.food   = None
-      self.water  = None
-      self.vision = 7
+        # Scripted hooks
+        self.target = None
+        self.food = None
+        self.water = None
+        self.vision = 7
 
-      #Submodules
-      self.skills = Skills(self)
+        # Submodules
+        self.skills = Skills(self)
 
-      self.diary  = None
-      if tasks := realm.config.TASKS:
-          self.diary = Diary(tasks)
+        self.diary = None
+        if tasks := realm.config.TASKS:
+            self.diary = Diary(tasks)
 
-      self.dataframe.init(nmmo.Serialized.Entity, self.entID, self.pos)
+        self.dataframe.init(nmmo.Serialized.Entity, self.entID, self.pos)
 
-   @property
-   def serial(self):
-      return self.population, self.entID
+    @property
+    def serial(self):
+        return self.population, self.entID
 
-   @property
-   def isPlayer(self) -> bool:
-      return True
+    @property
+    def isPlayer(self) -> bool:
+        return True
 
-   @property
-   def population(self):
-      return self.pop
+    @property
+    def population(self):
+        return self.pop
 
-   def applyDamage(self, dmg, style, stealing_enabled=True):
-      if dmg > 0 and stealing_enabled:
-         self.resources.food.increment(dmg)
-         self.resources.water.increment(dmg)
-      self.skills.applyDamage(dmg, style)
-      
-   def receiveDamage(self, source, dmg, stealing_enabled=True):
-      if not super().receiveDamage(source, dmg):
-         if source:
-            source.history.playerKills += 1
-         return 
-      if dmg > 0 and stealing_enabled:
-         self.resources.food.decrement(dmg)
-         self.resources.water.decrement(dmg)
-      self.skills.receiveDamage(dmg)
+    def applyDamage(self, dmg, style, stealing_enabled=True):
+        if dmg > 0 and stealing_enabled:
+            self.resources.food.increment(dmg)
+            self.resources.water.increment(dmg)
+        self.skills.applyDamage(dmg, style)
 
-   def receiveLoot(self, loadout):
-      if loadout.chestplate.level > self.loadout.chestplate.level:
-         self.loadout.chestplate = loadout.chestplate
-      if loadout.platelegs.level > self.loadout.platelegs.level:
-         self.loadout.platelegs = loadout.platelegs
+    def receiveDamage(self, source, dmg, stealing_enabled=True):
+        if not super().receiveDamage(source, dmg):
+            if source:
+                source.history.playerKills += 1
+            return
+        if dmg > 0 and stealing_enabled:
+            self.resources.food.decrement(dmg)
+            self.resources.water.decrement(dmg)
+        self.skills.receiveDamage(dmg)
 
-   def packet(self):
-      data = super().packet()
+    def receiveLoot(self, loadout):
+        if loadout.chestplate.level > self.loadout.chestplate.level:
+            self.loadout.chestplate = loadout.chestplate
+        if loadout.platelegs.level > self.loadout.platelegs.level:
+            self.loadout.platelegs = loadout.platelegs
 
-      data['entID']    = self.entID
-      data['annID']    = self.population
+    def packet(self):
+        data = super().packet()
 
-      data['base']     = self.base.packet()
-      data['resource'] = self.resources.packet()
-      data['skills']   = self.skills.packet()
+        data['entID'] = self.entID
+        data['annID'] = self.population
 
-      return data
-  
-   def update(self, realm, actions):
-      '''Post-action update. Do not include history'''
-      super().update(realm, actions)
+        data['base'] = self.base.packet()
+        data['resource'] = self.resources.packet()
+        data['skills'] = self.skills.packet()
 
-      if not self.alive:
-         return
+        return data
 
-      self.resources.update(realm, self, actions)
-      self.skills.update(realm, self, actions)
+    def update(self, realm, actions):
+        '''Post-action update. Do not include history'''
+        super().update(realm, actions)
 
-      if self.diary:
-         self.diary.update(realm, self)
+        if not self.alive:
+            return
+
+        self.resources.update(realm, self, actions)
+        self.skills.update(realm, self, actions)
+
+        if self.diary:
+            self.diary.update(realm, self)
