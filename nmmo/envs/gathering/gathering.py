36d2236485145cc613d5c3e299e2112aedd14325
ArgentumWalker <svidchen-ko@mail.ru>
2023-03-13 01:11:19 +0300

    Aftermerge fixes

--- nmmo/envs/gathering/gathering.py
+++ nmmo/envs/gathering/gathering.py
@@ -68,6 +68,7 @@ class ObscuredAndExclusiveGatheringConfig(GatheringConfig):
         self.PATH_MAPS = f'maps/gathering_obscured_and_exclusive_{self.MAP_WIDTH}x{self.MAP_HEIGHT}'
 
 
-class GatheringBuildingConfig(Small, Resource, Building):
-    TRAIN_HORIZON = HORIZON
-    EVAL_HORIZON = HORIZON
+class BuildingGatheringConfig(GatheringConfig, Building):
+    def __init__(self, n_groups, agents_per_group):
+        super().__init__(n_groups, agents_per_group)
+        self.PATH_MAPS = f'maps/gathering_obscured_and_exclusive_{self.MAP_WIDTH}x{self.MAP_HEIGHT}'
--- nmmo/envs/privacy_gathering/privacy_gathering.py
+++
@@ -1,9 +0,0 @@
-from nmmo.config import Resource, Small
-
-HORIZON = 70
-
-
-class GatheringConfig(Small, Resource):
-    TRAIN_HORIZON = HORIZON
-    EVAL_HORIZON = HORIZON
-
