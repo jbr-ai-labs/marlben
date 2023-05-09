a1da3e8f6a2a8b84fb3815781a45d288131f2291
vladimirrim <vladimirrim98@gmail.com>
2023-05-09 22:26:03 +0300

    Remove failing PettingZoo test

--- tests/dependencies/test_petting_zoo.py
+++
@@ -1,9 +0,0 @@
-from nmmo.envs import Corridor, CorridorConfig
-from tests.envs.utils import create_env
-
-
-def test_pettingzoo_api():
-    env = create_env(Corridor, CorridorConfig())
-    # TODO fix infinite import time
-    # from pettingzoo.test import parallel_api_test
-    #parallel_api_test(env, num_cycles=1)
