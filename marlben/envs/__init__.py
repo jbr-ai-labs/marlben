208d2800b225c439386f2c18a3b76aba879776db
vladimirrim <vladimirrim98@gmail.com>
2023-05-21 22:20:17 +0300

    Add gym registration

--- marlben/envs/__init__.py
+++ marlben/envs/__init__.py
@@ -1,8 +1,10 @@
+from marlben.config import Planting
 from marlben.envs.corridor import ScriptedCorridorConfig, OneNeuralCorridorConfig, CorridorConfig, Corridor
 from marlben.envs.boss_fight import BossFightConfig, BossFight
 from marlben.envs.gathering import GatheringConfig, GatheringConfigScripted, Gathering
 from marlben.envs.building import BuildingGatheringConfig, BuildingGatheringConfigScripted, GatheringBuilding
 from marlben.envs.planting import PlantingGatheringConfig, PlantingGatheringConfigScripted, GatheringPlanting
+from marlben.envs.raid import Raid
 from marlben.envs.spying import ObscuredGatheringConfig, GatheringObscured
 from marlben.envs.colors import ExclusiveGatheringConfig, GatheringExclusive
 from marlben.envs.exploring import ObscuredAndExclusiveGatheringConfig, GatheringObscuredAndExclusive
@@ -12,6 +14,7 @@ from marlben.envs.deathmatch import DeathmatchConfig, DeathmatchConfigScripted, 
 from marlben.envs.team_deathmatch import TeamDeathmatchConfig, TeamDeathmatchConfigScripted, TeamDeathmatch
 from marlben.envs.arena import ArenaConfig, ArenaConfigScripted, Arena
 from marlben.envs.predator_prey import PredatorPreyConfig, PredatorPreyConfigScripted, PredatorPrey
+from gym.envs.registration import register
 
 
 name2config = {
@@ -40,3 +43,29 @@ name2config = {
     "PredatorPrey": PredatorPreyConfig,
     "PredatorPreyScripted": PredatorPreyConfigScripted
 }
+
+name2env = {
+    'Corridor': Corridor,
+    'Gathering': Gathering,
+    'Arena': Arena,
+    'Pve': Pve,
+    'Deathmatch': Deathmatch,
+    'TeamDeathmatch': TeamDeathmatch,
+    'BossFight': BossFight,
+    'Building': GatheringBuilding,
+    'Colors': GatheringExclusive,
+    'Exploring': GatheringObscuredAndExclusive,
+    'Spying': GatheringObscured,
+    'Raid': Raid,
+    'Siege': Siege,
+    'Planting': Planting,
+    'PredatorPrey': PredatorPrey
+}
+
+for key in name2env.keys():
+    register(
+        id='MARLBEN-' + key + '-v1',
+        entry_point=name2env[key]
+    )
+
+
