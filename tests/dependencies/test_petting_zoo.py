d58fdf7c1b6007abcf0cf1c63e0ac9948b96765e
vladimirrim <vladimirrim98@gmail.com>
2023-05-21 22:25:08 +0300

    Add missing tests

---
+++ tests/dependencies/test_petting_zoo.py
@@ -0,0 +1,8 @@
+from nmmo.envs import Corridor, CorridorConfig
+from pettingzoo.test import parallel_api_test
+from tests.envs.utils import create_env
+
+
+def test_pettingzoo_api():
+    env = create_env(Corridor, CorridorConfig())
+    parallel_api_test(env, num_cycles=1000)
