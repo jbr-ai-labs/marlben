7a76d2b8c51cbf69279ae1649e545b81c63bd9a8
Vladimir Egorov <vladimirrim98@gmail.com>
2023-06-10 12:23:44 +0200

    Update team deathmatch description

--- marlben/envs/team_deathmatch/env.py
+++ marlben/envs/team_deathmatch/env.py
@@ -1,4 +1,14 @@
 import marlben
 
+"""
+As the name suggests, this environment is the team version of the deathmatch environment. 
+Agents are divided into multiple teams. The attack action has been modified so that its effect can only be applied to an agent from the other team.
+
+Expected behavior: 
+Naive agents can behave similarly to Arena or Deathmatch environments, ignoring team affiliation. 
+A more profitable strategy is to coordinate with your team members to attack opponents, thus providing additional rewards and reducing competition for resources.
+
+Implementation details are located in config file
+"""
 class TeamDeathmatch(marlben.Env):
     pass
