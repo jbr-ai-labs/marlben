70ae1df81bbfe83c9651f82f9164eb1f6a7e03b2
Vladimir Egorov <vladimirrim98@gmail.com>
2023-06-10 12:01:08 +0200

    Update building description

--- marlben/envs/building/env.py
+++ marlben/envs/building/env.py
@@ -1,5 +1,13 @@
 import marlben
 
+"""
+Agents are given an additional building action. Its effect is that the cell where the agent was in the previous turn is replaced with a stone cell.
 
+Expected behavior:
+Agents can use their already collected resources to trap other agents in parts of the map, or fence themselves off on parts of the map with forest and water tiles.
+Thus, the division of the territory between agents can occur.
+
+Implementation details are located in config file
+"""
 class GatheringBuilding(marlben.Env):
     pass
