c0aa6f47c31f04663c17b695cc0556a859f3557f
Vladimir Egorov <vladimirrim98@gmail.com>
2023-06-10 12:19:21 +0200

    Update colors description

--- marlben/envs/colors/env.py
+++ marlben/envs/colors/env.py
@@ -1,5 +1,13 @@
 import marlben
 
+"""
+This environment is a simplified version of the Exploring environment. In it, all information about resource cells is initially available to all agents. 
+As a consequence, there is no need to exchange information. This environment is suitable for testing and debugging algorithms before moving on to a more complex Exploring environment.
 
+Expected behavior:
+Agents collect resources of their color to survive on the map.
+
+Implementation details are located in config file
+"""
 class GatheringExclusive(marlben.Env):
     pass
